e8bd6acc0110b7049f148ebf3809c3a9b5d00f7b
Vincent Nalupta <vincent.nalupta@jcrew.com>
2014-12-18 19:14:43 -0500

    added animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,14 +28,16 @@ var Portfolio = function(jsonIn, selectedIn, elIn, idIn, captionIn){
   var setSelectedItem = function(project){
     defaults._selected = project;    
   }
-  var swapSelectedItem = function(oldProject, newProject){
-    defaults._list[getListIndex(oldProject)].classList.remove('selected');    
-    defaults._list[getListIndex(newProject)].classList.add('selected');    
-  }
   var swapSelectedImg = function(newProject){
+    Portfolio.prototype.fadeOut(defaults._img)
     defaults._img.src = defaults._json[newProject].img
     defaults._img.parentNode.href = defaults._json[newProject].url;
     defaults._caption.innerHTML = defaults._json[newProject].caption;
+    Portfolio.prototype.fadeIn(defaults._img)
+  }  
+  var swapSelectedItem = function(oldProject, newProject){
+    defaults._list[getListIndex(oldProject)].classList.remove('selected');    
+    defaults._list[getListIndex(newProject)].classList.add('selected');    
   }
   var getListIndex = function(projectName){ 
     // returns index of project in _list
@@ -47,9 +49,13 @@ var Portfolio = function(jsonIn, selectedIn, elIn, idIn, captionIn){
     }
   }
   var attachEvents = function(){    
+    // attach clicks to project list
     for(var i=0, l=defaults._list.length; i < l; i++){      
       defaults._list[i].addEventListener('click', clickHandler, false)
     }    
+    // hovers for caption
+    defaults._img.addEventListener('mouseover', mouseOverHandler, false);
+    defaults._img.addEventListener('mouseout', mouseOutHandler, false);
   }
   var clickHandler = function(e){    
     var clickedProj = e.target.dataset.project.toString();         
@@ -57,7 +63,15 @@ var Portfolio = function(jsonIn, selectedIn, elIn, idIn, captionIn){
     swapSelectedItem(defaults._selected, clickedProj)    // add selected class        
     setSelectedItem(clickedProj) // update defaults
   }  
-  
+  // TODO: might wanna curry these two
+  var mouseOverHandler = function(e){
+    e.stopPropagation();    
+    Portfolio.prototype.fadeIn(defaults._caption)
+  }
+  var mouseOutHandler = function(e){
+    e.stopPropagation();    
+    Portfolio.prototype.fadeOut(defaults._caption)
+  }  
 
   // PRIVILEGED METHODS
   this.getProjects = function(){
@@ -82,4 +96,33 @@ var Portfolio = function(jsonIn, selectedIn, elIn, idIn, captionIn){
 
 // PUBLIC METHODS
 Portfolio.prototype = {
+  // TODO: curry these two also
+  fadeIn : function(el) {  
+    el.style.opacity = 0;
+
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity + (new Date() - last) / 200;
+      last = +new Date();
+
+      if (+el.style.opacity < 1) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+      }
+    };
+    tick();
+  },
+  fadeOut : function(el) {
+    el.style.opacity = 1;
+
+    var last = +new Date();
+    var tick = function() {
+      el.style.opacity = +el.style.opacity - (new Date() - last) / 200;
+      last = +new Date();
+
+      if (+el.style.opacity > 0) {
+        (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick, 16)
+      }
+    };
+    tick();
+  }  
 }
